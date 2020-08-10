80daba1f4a276aa731965bdd4672823feb8296ac
mmasso <mmasso@cifpfbmoll.eu>
2020-08-10 17:17:52 +0200

    feat(lorem_convert): simplificamos convertidor y añadimos funciones ad

--- src/lorem_convert.py
+++ src/lorem_convert.py
@@ -2,19 +2,26 @@ def text_to_string():
     file = open("src/lorem_ipsum.txt")
     lorem_string = file.read().replace("\n", " ")
     file.close()
-    lorem_sin_comas = quitar_comas(lorem_string)
-    return lorem_sin_comas
+    return lorem_string
 
 
 def quitar_comas(lorem_ipsum):
     lorem_sin_comas = lorem_ipsum.replace(",", "")
     return lorem_sin_comas
 
+
 def quitar_puntos(lorem_ipsum):
     lorem_sin_punto = lorem_ipsum.replace(".", "")
     return lorem_sin_punto
 
+
+def quitar_signos_parrafo(lorem_ipsum):
+    lorem_sin_parrafo = lorem_ipsum.replace("/", "")
+    return lorem_sin_parrafo
+
+
 def quitar_puntuacion(lorem_ipsum):
     lorem_sin_comas = quitar_comas(lorem_ipsum)
-    lorem_sin_puntuacion = quitar_puntos(lorem_sin_comas)
+    lorem_sin_puntos_comas = quitar_puntos(lorem_sin_comas)
+    lorem_sin_puntuacion = quitar_signos_parrafo(lorem_sin_puntos_comas)
     return lorem_sin_puntuacion
