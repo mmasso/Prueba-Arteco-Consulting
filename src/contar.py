875ba8ab5bdde0674e0515dcda81cf4907f3955b
mmasso <mmasso@cifpfbmoll.eu>
2020-08-10 18:30:23 +0200

    feat(contar.py): contar con identificacion palabras y n

--- src/contar.py
+++ src/contar.py
@@ -22,14 +22,22 @@ def contar_palindromos(lorem_ipsum):
 def es_palindromo(palabra):
     return palabra.lower() == palabra.lower()[::-1]
 
+    
 
-def contar_palabras_repetidas(lorem_ipsum):
-    dicc_lorem = crear_dict_repetidas(lorem_ipsum)
+def representar_lista_valores(dicc_lorem, lista_keys_repetidas):
+    string_final = "Las palabras más repetidas són:"
+    for key in lista_keys_repetidas:
+        string_final += " '" + key + "' con " + str(dicc_lorem.get(key)) + " repeticiones"
+    return string_final
+
+
+def lista_llaves_mas_repetidas(dicc_lorem):
     i = 0
     lista_mas_repetidas = list()
+    dicc_lorem_copy = dicc_lorem.copy()
     while i < 5:
-        lista_mas_repetidas.append(max(dicc_lorem, key=dicc_lorem.get))
-        dicc_lorem.pop(max(dicc_lorem, key=dicc_lorem.get))
+        lista_mas_repetidas.append(max(dicc_lorem_copy, key=dicc_lorem_copy.get))
+        dicc_lorem_copy.pop(max(dicc_lorem_copy, key=dicc_lorem_copy.get))
         i += 1
     return lista_mas_repetidas
 
@@ -45,3 +53,10 @@ def crear_dict_repetidas(lorem_ipsum):
         else:
             dicc_lorem[palabra] = 1
     return dicc_lorem
+
+
+def contar_palabras_repetidas(lorem_ipsum):
+    dicc_lorem = crear_dict_repetidas(lorem_ipsum)
+    lista_keys_repetidas = lista_llaves_mas_repetidas(dicc_lorem)
+    string_lista_valores = representar_lista_valores(dicc_lorem, lista_keys_repetidas)
+    return string_lista_valores
