dd81cb797456cc1dc1554b9de7a417d19ea5ad1c
mmasso <mmasso@cifpfbmoll.eu>
2020-08-10 17:18:45 +0200

    feat(main): palindromos work

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
-from lorem_convert import text_to_string
+from lorem_convert import text_to_string, quitar_puntuacion
 from contar import contar_frase, contar_parrafos, contar_palabras, contar_palindromos
 
 
 def main():
     lorem_ipsum = text_to_string()
+    lorem_sin_puntuacion = quitar_puntuacion(lorem_ipsum)
     n_parrafos = contar_parrafos(lorem_ipsum)
     n_frases = contar_frase(lorem_ipsum)
-    n_palabras = contar_palabras(lorem_ipsum)
-    n_palindromos = contar_palindromos(lorem_ipsum)
+    n_palabras = contar_palabras(lorem_sin_puntuacion)
+    n_palindromos = contar_palindromos(lorem_sin_puntuacion)
     ### n_palabras_repetidas = contar_palabras_repetidas(lorem_ipsum)
     ### n_tuplas_repetidas = contar_tuplas_repetidas(lorem_ipsum)
     return n_parrafos, n_frases, n_palabras, n_palindromos
