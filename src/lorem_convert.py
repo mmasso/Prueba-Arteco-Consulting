8bcc8a016aab1cecf664474bd676c8c904072654
mmasso <mmasso@cifpfbmoll.eu>
2020-08-12 20:21:14 +0200

    refactor(contar.py, main.py, lorem_convert.py): refactor variable names y codigo comentado

--- src/lorem_convert.py
+++ src/lorem_convert.py
@@ -1,27 +1,7 @@
+# Tratamos el txt obtenido para convertiro en una string que
+# Python pueda leer y operar mas facilmente
 def text_to_string():
-    file = open("src/lorem_ipsum.txt")
+    file = open("resources/lorem_ipsum.txt")
     lorem_string = file.read().replace("\n", " ")
     file.close()
     return lorem_string
-
-
-def quitar_comas(lorem_ipsum):
-    lorem_sin_comas = lorem_ipsum.replace(",", "")
-    return lorem_sin_comas
-
-
-def quitar_puntos(lorem_ipsum):
-    lorem_sin_punto = lorem_ipsum.replace(".", "")
-    return lorem_sin_punto
-
-
-def quitar_signos_parrafo(lorem_ipsum):
-    lorem_sin_parrafo = lorem_ipsum.replace("/", "")
-    return lorem_sin_parrafo
-
-
-def quitar_puntuacion(lorem_ipsum):
-    lorem_sin_comas = quitar_comas(lorem_ipsum)
-    lorem_sin_puntos_comas = quitar_puntos(lorem_sin_comas)
-    lorem_sin_puntuacion = quitar_signos_parrafo(lorem_sin_puntos_comas)
-    return lorem_sin_puntuacion
--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
-from lorem_convert import text_to_string, quitar_puntuacion
-from contar import contar_frase, contar_parrafos, contar_palabras, contar_palindromos, contar_palabras_repetidas, contar_tuplas_repetidas
+# Importamos las funciones necesarias de los otros modulos
+from lorem_convert import text_to_string
+from contar import (contar_frase, contar_parrafos, contar_palabras,
+                    contar_palindromos, contar_palabras_repetidas,
+                    contar_tuplas_repetidas)
 
 
+# El main del proyecto, donde juntamos todo
 def main():
-    lorem_ipsum = text_to_string()
-    lorem_sin_puntuacion = quitar_puntuacion(lorem_ipsum)
-    n_parrafos = contar_parrafos(lorem_ipsum)
-    n_frases = contar_frase(lorem_ipsum)
-    n_palabras = contar_palabras(lorem_sin_puntuacion)
-    n_palindromos = contar_palindromos(lorem_sin_puntuacion)
-    n_palabras_repetidas = contar_palabras_repetidas(lorem_sin_puntuacion)
-    n_tuplas_repetidas = contar_tuplas_repetidas(lorem_ipsum)
-    return n_parrafos + "\n" +  n_frases + "\n" + n_palabras + "\n" + n_palindromos + "\n" + n_palabras_repetidas + "\n" + n_tuplas_repetidas
+    texto = text_to_string()
+    n_parrafos = contar_parrafos(texto)
+    n_frases = contar_frase(texto)
+    n_palabras = contar_palabras(texto)
+    n_palindromos = contar_palindromos(texto)
+    n_palabras_repetidas = contar_palabras_repetidas(texto)
+    n_tuplas_repetidas = contar_tuplas_repetidas(texto)
+    return n_parrafos + "\n" + n_frases + "\n" + n_palabras + "\n" + \
+        n_palindromos + "\n" + n_palabras_repetidas + "\n" + n_tuplas_repetidas
 
 
 if __name__ == "__main__":
     print(main())
-
